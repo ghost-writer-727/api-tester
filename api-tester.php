5c17ebe0f2c5da1b1d487edf9801624270f88988
Ghost Writer <4065416+ghost-writer-727@users.noreply.github.com>
2025-03-19 17:20:11 +0000

    Define plugin header

---
+++ api-tester.php
@@ -0,0 +1,11 @@
+<?php namespace API_Tester;
+defined( 'ABSPATH' ) || exit;
+/**
+ * Plugin Name: API Tester
+ * Description: A plugin to test APIs calls.
+ * Version: 0.0.1
+ * Author: Ghostwriter
+ */
+
+
+ 
