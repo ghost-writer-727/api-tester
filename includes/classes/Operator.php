b249653ab2d02960141eb2651751cc22c65e7bf5
Ghost Writer <4065416+ghost-writer-727@users.noreply.github.com>
2025-03-20 15:43:40 +0000

    Build a class for interacting with APIs

---
+++ includes/classes/Operator.php
@@ -0,0 +1,95 @@
+<?php namespace API_Tester;
+defined( 'ABSPATH' ) || exit;
+
+class Operator{
+    public $endpoint;
+    public $method;
+    public $route;
+    public $timeout = 5;
+    public $redirection = 5;
+    public $httpversion = '1.0';
+    public $user_agent; // Defaults to get_bloginfo( 'url' )
+    public $reject_unsafe_urls = false;
+    public $blocking = true;
+    public $headers;
+    public $cookies;
+    public $body;
+    public $compress = false;
+    public $decompress = true;
+    public $sslverify = true;
+    public $sslcertificates; // Absolute path to an SSL certificate file. Defaults to Wordpress defaults when empty.
+    public $stream = false;
+    public $filename = null; // Filename of the file to save the streaed response.
+    public $limit_response_size = null; // Size in bytes to limit the response to.
+
+    public $response;
+    public $error;
+
+    public function __construct(){
+        $this->user_agent = get_bloginfo( 'url' );
+    }
+
+    private function args(){
+        $args = [];
+        foreach( [ 
+            'timeout', 
+            'redirection', 
+            'httpversion', 
+            'user_agent', 
+            'reject_unsafe_urls', 
+            'blocking', 
+            'headers', 
+            'cookies', 
+            'body', 
+            'compress', 
+            'decompress', 
+            'sslverify', 
+            'sslcertificates', 
+            'stream', 
+            'filename', 
+            'limit_response_size' 
+        ] as $arg ){
+            if( isset( $this->$arg ) && ! empty( $this->$arg ) ){
+                $args[$arg] = $this->$arg;
+            }
+        }
+        return $args;
+    }
+
+    public function get() {
+        return $this->request('GET');
+    }
+
+    public function post() {
+        return $this->request('POST');
+    }
+
+    public function request($method) {
+        $url = $this->endpoint . $this->route;
+        $args = $this->args();
+        $args['method'] = strtoupper($method); // Ensure method is uppercase
+
+        $this->response = wp_remote_request($url, $args);
+
+        return $this->process_response();
+    }
+
+    private function process_response() {
+        if (is_wp_error($this->response)) {
+            $this->error = $this->response->get_error_message();
+            return [
+                'error' => $this->error,
+                'response' => null,
+                'status_code' => null,
+                'args' => $this->args()
+            ];
+        }
+
+        return [
+            'error' => null,
+            'response' => wp_remote_retrieve_body($this->response), // Extract response body
+            'status_code' => wp_remote_retrieve_response_code($this->response),
+            'args' => $this->args()
+        ];
+    }
+}
